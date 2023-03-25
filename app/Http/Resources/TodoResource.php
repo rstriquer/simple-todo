c5c3bdf08672ec63372c0fd06ea54663bd6f5a61
rstriquer <rstriquer@gmail.com>
2023-03-25 20:25:57 -0300

    feat: Add filterInput method to TodoResource mainly to filter Done Field

--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TodoResource extends JsonResource
@@ -15,5 +16,31 @@ class TodoResource extends JsonResource
     public function toArray($request)
     {
         return parent::toArray($request);
+    }
+
+    /**
+     * normalize request inputted parameters
+     */
+    public static function filterInput(Request $request) : array
+    {
+        $payload = $request->input();
+
+        if (isset($payload['done'])) {
+            $payload['done'] = self::filterDoneField($payload['done']);
+        }
+        
+        return $payload;
+    }
+
+    /**
+     * Return true or false according to "done" field
+     * @see \App\Requests\CreateTodo
+     */
+    private static function filterDoneField(string $done) : int
+    {
+        if (in_array($done, ['yes', 'true', '1'])) {
+            return true;
+        }
+        return false;
     }
 }
