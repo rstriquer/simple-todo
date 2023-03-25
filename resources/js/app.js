ffc8991bc655475bfcc09d4a6e68e56223c0ce27
rstriquer <rstriquer@gmail.com>
2023-03-25 18:28:25 -0300

    style: remove unnecessary comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,4 @@ import { createApp } from "vue"
 import app from "./components/app.vue"
 import router from "./router/index.js"
 
-// app = createApp()
-// app.component("todo-list-grid", TodoListGrid)
-// app.mount("#app")
-
 createApp(app).use(router).mount("#app")
